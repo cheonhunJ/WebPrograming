e32c6270e24a806365ac24ecf0097ac8a2e13c76
cheonhunJ <103979003+cheonhunJ@users.noreply.github.com>
2025-05-19 18:29:13 +0900

    Add files via upload

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -4,11 +4,14 @@ from .views import map_view
 
 urlpatterns = [
     path('', views.home, name='home'),
+
     path('login/',views.login, name='login'),
+
     path('busmap/', views.busmap, name='busmap'),
 
-    path('map/', views.map, name='map'),
 
-    path('map/', map_view, name='map'),
+    path('map/', views.map_view, name='map'),
+
+    path('info/', views.info, name='info'),
 
 ]
